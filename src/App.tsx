4d231fd5134db5258fc486bae352562329689ca5
André Dias <andre25dias@gmail.com>
2023-08-20 15:03:03 -0300

    estilizando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import PrimaryInputProps from './components/Input/PrimaryInput'
 
 function App() {
+  const [email, setEmail] = useState("")
+
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <PrimaryInputProps value={email} onChange={event => setEmail(event.target.value)} name='email' label='Digite o seu email'></PrimaryInputProps>
+    </div>
   )
 }
 
--- src/components/Input/PrimaryInput.tsx
+++ src/components/Input/PrimaryInput.tsx
@@ -1,14 +1,24 @@
 import { Input } from "@chakra-ui/react";
+import {ChangeEventHandler} from "react";
+import "./primary-input.css"
 
 interface PrimaryInputProps{
     name: string,
     value: string,
-    onChange: 
-
+    onChange: ChangeEventHandler<HTMLInputElement>
+    label: string
 }
 
-export default function PrimaryInputProps({}: PrimaryInputProps){
+
+export default function PrimaryInputProps({name, value, onChange, label}: PrimaryInputProps){
     return(
-        <Input variant='filled' placeholder='Filled' name={}/>
+        <div className="input-container">
+            <label className="label">{label}</label>
+            <Input variant='filled' 
+            placeholder='Digite seu nome' 
+            name={name} 
+            value={value} 
+            onChange={onChange}/>
+        </div>
     )
 }
